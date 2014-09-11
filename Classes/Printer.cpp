dd16312752f44af532307964b32d3058d7fbb350
sebaslv15 <sebaslv12@hotmail.com>
2014-09-11 17:00:56 -0500

    Function to print dates needs to print Tzolkin

--- Classes/Printer.cpp
+++ Classes/Printer.cpp
@@ -6,14 +6,31 @@ using namespace std;
 
 Printer::Printer(map<string,int>* pHAAB, map<string,int>* pTzolkin)
 {
-  HAAB = pHAAB;
-  Tzolkin = pTzolkin;
+  Printer::HAAB = pHAAB;
+  Printer::Tzolkin = pTzolkin;
 }
 
 Printer::~Printer(){
-
+  
 }
 
 void Printer::printDate(bool type,long date){
   //TODO
+  if(type){
+    int year = date / 365;
+    int rest = date % 365;
+    int month = rest / 20;
+    int day = rest % 20;
+    string monthS;
+    map<string, int>::iterator it;
+    for(it = HAAB->begin(); it != HAAB->end();++it){
+      if(it->second == month){
+	monthS = it -> first;
+	it = HAAB->end();
+      }
+    }
+    cout << "" + day + " " + monthS + " " + year << endl;
+  }else{
+
+  }
 }
